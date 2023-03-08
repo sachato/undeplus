15b4e28c6e2ef85bd305ca9b6f85504e51de0b27
Florian <florian@ROGER.localdomain>
2023-03-08 14:08:43 +0100

    first simple cd commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors = require('cors')
+
+// PARAM API
+const app = express()
+
+app.use(cors())
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// PARAM ROUTER
+/******************************/
+/*** Mise en place du routage */
+
+app.get('/', (req, res) => res.send(`I'm online. All is OK !`))
+
+
+app.get('*', (req, res) => res.status(501).send('What the hell are you doing !?!'))
+
+
+// START API SERVER
+
+app.listen(process.env.SERVER_PORT, () => {
+    console.log(`This server is running on port ${process.env.SERVER_PORT}. Have fun !`)
+})
