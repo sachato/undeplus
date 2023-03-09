dee84ceb393cd8d1e18df7c8501fd1333a41ca72
Floriansp40 <62279896+Floriansp40@users.noreply.github.com>
2023-03-09 10:41:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,10 +9,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-let a = 1
-while(true){
-    a = a*100
-}
+
 
 // PARAM ROUTER
 /******************************/
